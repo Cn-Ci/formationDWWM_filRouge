450173d08495bb29995a72445873ac24a9501876
L_Perrine <perrine.leguai@gmail.com>
2020-10-16 15:04:53 +0200

    inscription

---
+++ inscription.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <!--BOOTSTRAP-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+     <!-- TYPO -->
+    <link href="//db.onlinewebfonts.com/c/7a8bc7c29b5bcadb9510cca51210ac46?family=Sitka+Banner" rel="stylesheet" type="text/css" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="inscription.css">
+</head>
+
+<body>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+   Inscription
+  </button>
+
+    <!-- Modal -->
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header ">
+                    <h5 class="modal-title " id="exampleModalLabel">Espace perso</h5>
+                    <button type="button" class="close pt-2" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+                </div>
+                <div class="globalConnexion text-center">
+                    <!-- image -->
+                    <div id="imageCo">
+
+
+                    
+                        <!-- JAVAAAAAAAA -->
+                        <script LANGUAGE="JavaScript">
+                            <!--
+                            document.write("<body>");
+                            var tab = new Array("../image/icone-inscription/icone1.jpg","../image/icone-inscription/icone2.jpg","../image/icone-inscription/icone3.jpg", "../image/icone-inscription/icone4.jpg", "../image/icone-inscription/icone5.jpg", "../image/icone-inscription/icone6.jpg");
+                            //remplacez aaa, bbb et ccc par vos images
+
+                            var n = parseInt( Math.random() * tab.length)
+                            while (isNaN(n)) {
+                            n = parseInt( Math.random() * tab.length)
+                            }
+
+                            document.write("<img src='../image/icone_inscription/' " + tab[n] + "' alt='Image aléatoire'>");
+                            //-->
+                        </script> 
+
+
+
+                       <!-- IMAAAAAAAAAAAAAAAAAAGE -->
+                        <img src="img/icone1.jpg" class="img-fluid">
+                    </div>
+                    <!-- identifiant et mdp -->
+                    <div class="identifantsCo justify-content-center pt-3 ">
+                        <!-- identifiant -->
+                        <div class="identifiants justify-content-center row">
+                            <div id="iconeID text-center">
+                                <img src="img/id.png">
+                            </div>
+                            <div class="formID ">
+                                <form action="formdatas.php?action=connexion" method="post">
+                                    <input type="text" name="identifiant" id="identifiant" alt="votre nom d'identifiant" placeholder="Identifiant" required>
+                            </div>
+                        </div>
+                        <!-- séparateur -->
+                        <div class="separateur " >
+                            
+                            <hr class="style1 color-228b22" id="separateur"></hr>
+                        </div>
+                        <!-- mot de passe -->
+                        <div class="identifiants pt-3 row justify-content-center" id="">
+                            <div id="iconeLOCK col-4">
+                                <img src="img/locker.png">
+                            </div>
+                            <div>
+                                <div class="formPW col-8">
+                                    <input type="password" id="pass" name="password"  alt="votre mot de passe" heigth="40px" pattern="(?=^.{8,30}$)((?=.*\d)|(?=.*\W+))(?![\n])(?=.*[A-Z])(?=.*[a-z]).*$" title="Un mot de passe de 8 à 30 caractères, comprenant un nombre ou un caractère spécial, une majuscule et une minuscule."
+                                        placeholder="*********" required>
+                                </div>
+                                <div>
+                                    <p> <a href="www.jaiperdumonmodp.com" class="text-success" id="mdpoublié"> Mot de passe oublié ? </a> </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    <div class="modal-footer">
+                    <button type="submit" class="btn btn-success color-228B22 mb-10" id="boutonsubmit"> Se connecter </button> </br>
+
+                        </form>
+                    <button type="submit" class="btn btn-success color-228B22" id="boutoninscription"> S'inscrire</button>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</body>
+
+
+</html>
