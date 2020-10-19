49098a7e795c3eb9f2ea1e7b091f1b9fa8bbe503
cindy <cindy.cnockaert@live.fr>
2020-10-19 08:57:33 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>MOBILI'T - Accueil</title>
+        <meta charset="utf-8">
+        <!-- BOOTSTRAP -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+        <link 
+            rel="stylesheet" 
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+            integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
+            crossorigin="anonymous">
+        <!-- JQUERY -->
+        <script
+            src         ="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity   ="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin ="anonymous">
+        </script>
+        <!-- CSS -->
+        <link 
+            rel="stylesheet" 
+            type="text/css" 
+            href="styleOrga.css">
+        <!-- TYPO -->
+        <link 
+            href="//db.onlinewebfonts.com/c/7a8bc7c29b5bcadb9510cca51210ac46?family=Sitka+Banner" 
+            rel="stylesheet"
+            type="text/css"/>
+
+            <!-- autres -->
+            <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+            <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    </head>
+
+
+    <footer>
+   
+
+        <!-- ligne de séparation -->
+        <hr class="hrsep">
+
+            <!-- image footer -->
+                <div id="foot" class="img-fluid w-100">
+
+
+    <!-- colonnes -->
+        <div class="contact " >
+            <div class="row">
+
+                <!-- colonne 1 -->
+                    <div class="col-12 col-lg-4 text-center p-2">
+                    Navigation
+                            <hr class="hrcon">
+                            <p></p>
+                            <a href="orga.php">
+                                Accueil
+                            </a>
+                            <p></p>
+                            <a href="orga.php">
+                                Organisation
+                            </a>
+                            </p>
+                            <p>
+                            <a href="#">
+                                Destinations
+                            </a>
+                            </p>
+                            <p>
+                            <a href="#">
+                                Forum
+                            </a>
+                            </p>
+                          
+                    </div>
+            
+
+                    <!-- colonne 2 -->
+                    <div class="col-12 col-lg-4 text-center p-2 text-black-100">
+                        Aide
+                            <hr class="hrcon">
+                                <p>
+                                    <a href="#">
+                                    Personnel
+                                    </a>
+                                </p>
+                                <p>
+                                    <a href="#">
+                                        Recherche
+                                    </a>
+                                </p>
+                                <p>
+                                    <a href="#">
+                                        s'inscrire
+                                    </a>
+                                </p>
+                                <p>
+                                    <a href="#">
+                                        Votre compte
+                                    </a>
+                                </p>
+                               
+                    </div>
+
+                    <!-- colonne 3 -->
+                    <div class="col-12 col-lg-4 text-center p-2 text-black-100">
+                            CONTACT
+                            <hr class="hrcon">
+                                    <p class="text-center">mobiliT</p>
+                                    <p class="text-center"><i class="fas fa-map-marker-alt pr-3"></i>79 Rue de Marseille, 75010 Paris</p>
+                                    <p class="text-center"><i class="fas fa-phone-square-alt pr-3"></i>0 123 456 789 </p>
+                                    <p class="text-center"><i class="fas fa-at pr-3"></i>mobiliT@pro.fr </p>
+                   </div>
+            </div>
+        </div>
+        </div>
+
+
+
+    </footer>
+
+
+<script 
+    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" 
+    crossorigin="anonymous">
+</script>
+<script 
+    src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
+    crossorigin="anonymous">
+</script>
+<script 
+    type="text/javascript" 
+    src="script.js">
+</script>
+</body>
+</html>
