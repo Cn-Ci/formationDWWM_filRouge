5393dae992ae46f53727bb6380c3b268d3a0ff3c
Maxime <lefebvremaximee@gmail.com>
2020-10-19 16:17:16 +0200

    modif timeline

--- orga.php
+++ orga.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <title>MOBILI'T - Accueil</title>
+        <title>MOBILI'T - Organisation</title>
         <meta charset="utf-8">
         <!-- BOOTSTRAP -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+        <link 
+            rel="stylesheet" 
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" 
+            integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" 
+            crossorigin="anonymous" />
         <link 
             rel="stylesheet" 
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
@@ -28,36 +32,35 @@
             type="text/css"/>
 
             <!-- autres -->
-            <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-            <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>
     
     <body>
-    <!-- Navbar -->
+        <!-- Navbar -->
         <div class="container-fluid">
             <?php include 'navbar.php';?>
         </div>
-        
 
-    <!-- timeline -->
-    <div id="plane" class="image text-center p-2">
-        <img src="img/avion.jpeg" class="img-fluid rounded">
-    </div>
+        <!-- timeline -->
+        <div id="plane" class="image text-center p-2">
+            <img src="img/avion.jpeg" class="img-fluid rounded">
+        </div>
 
-    <!-- timeline -->
+        <!-- timeline -->
         <div class="timeline p-5">
             <h4>L'organisation</h4>
             <div class="row">
                 <div class="col-md-12">
                     <div class="main-timeline">
-                        <a href="#" class="timeline">
-                                <div class="timeline-icon"><i class="fas fa-luggage-cart"></i></div>
-                                <div class="timeline-content">
-                                    <h3 class="title">Avant le voyage !</h3>
-                                    <p class="description">
-                                        Cette étape de la préparation est primordiale.
+                        <!--------------------------------------- ROSE ---------------------------------------------> 
+                        <div class="timeline">
+                            <div class="timeline-icon"><i class="fa fa-luggage-cart"></i></div>
+                            <div class="timeline-content">
+                                <h3 class="title">Avant le voyage !</h3>
+                                <p class="description">
+                                    Cette étape de la préparation est primordiale.
                                     </br>
                                     </br>
                                         Notre selection de lieux touristiques offrent des services spécialement dédiés aux PMR (personnes à mobilité réduite) comme des chambres spéciales, des rampes d’accès, des ascenseurs, etc...
@@ -73,60 +76,63 @@
                                         Verifiez auprès de votre assurance, que vous êtes assuré en cas de voyage.
                                     </br>
                                     </br>
-                                        <Strong>Pour tous renseignements, n'hésitez pas à contacter le personnel de mobiliT </strong>
-                                    </p>
-                                </div>
-                            </a>
+                                    <Strong>Pour tous renseignements, n'hésitez pas à contacter le personnel de mobiliT </strong>
+                                </p>
+                            </div>
+                        </div>
+                        <!--------------------------------------- ORANGE ---------------------------------------------> 
                         <a href="#" class="timeline">
                             <div class="timeline-icon"><i class="fa fa-rocket"></i></div>
                             <div class="timeline-content">
                                 <h3 class="title">Choisir son voyage !</h3>
                                 <p class="description">
                                     Il est important de savoir si vous pouvez vous transférer, vous lever, faire quelques pas ou s’il faut une assistance jusqu’au siège de l’appareil. 
-                                </br>
-                                </br>
-                                    Tous les fauteuils roulants, manuels ou électriques voyagent en soute au même titre qu’une valises si votre voyage nécessite le trajet en avion.
-                                </br>
-                                </br>
-                                    Nos destinations sont préalablement choisis pour que vous puissiez accéder a toute les activités proposés. 
-                                </br>
-                                </br>
-                                    L'ensemble des activités proposés dans les différentes destinations sont concus pour des PMR.
-                                </br>
-                                </br>
+                                    </br>
+                                    </br>
+                                        Tous les fauteuils roulants, manuels ou électriques voyagent en soute au même titre qu’une valises si votre voyage nécessite le trajet en avion.
+                                    </br>
+                                    </br>
+                                        Nos destinations sont préalablement choisis pour que vous puissiez accéder a toute les activités proposés. 
+                                    </br>
+                                    </br>
+                                        L'ensemble des activités proposés dans les différentes destinations sont concus pour des PMR.
+                                    </br>
+                                    </br>
                                     <Strong>Pour connaitre le retour d'expérience des voyageurs, n'hésitez pas à consulter les avis des différentes destination et le forum de mobiliT </strong> 
                                 </p>
                             </div>
                         </a>
+                        <!--------------------------------------- VERT ---------------------------------------------> 
                         <a href="#" class="timeline">
                             <div class="timeline-icon"><i class="fa fa-globe"></i></div>
                             <div class="timeline-content">
                                 <h3 class="title">Pendant le voyage !</h3>
                                 <p class="description">
                                     Le personnel mobiliT est à votre disposition pour répondre a vos craintes et ainsi vous conseillez afin de surveiller votre maison ou appartement, ou encore pour la garde de vos animaux de compagnie.
-                                </br>
-                                </br>
-                                    Nous vous conseillons de profiter un maximum de votre voyage, et pour profiter pleinement de toute les activités, n'hésitez pas à signaler vos différentes pathologies afin d'être aider et informer des diiférentes alternatives proposés.
-                                </br>
-                                </br>
+                                    </br>
+                                    </br>
+                                        Nous vous conseillons de profiter un maximum de votre voyage, et pour profiter pleinement de toute les activités, n'hésitez pas à signaler vos différentes pathologies afin d'être aider et informer des diiférentes alternatives proposés.
+                                    </br>
+                                    </br>
                                     <Strong>Pour toutes demandes, n'hésitez pas à vous rapprochez du personnel de votre centre de voyage des destinations proposés par mobiliT </strong> 
                                 </p>
                             </div>
                         </a>
+                        <!--------------------------------------- BLEU ---------------------------------------------> 
                         <a href="#" class="timeline">
                             <div class="timeline-icon"><i class="fa fa-briefcase"></i></div>
                             <div class="timeline-content">
                                 <h3 class="title">Après le voyage</h3>
                                 <p class="description">
-                                </br>
-                                    Même après votre voyage, le personnel de mobiliT reste a votre écoute, en cas d'oublie, ou d'imprevu subi lors de votre voyage.  
-                                </br>
-                                </br>
-                                    Nous espérons que votre voyage s'est très bien passé et que vous avez été satisfait de nos renseignements, conseils, ainsi que nos destinations,
-                                    c'est pour cela que moibiliT souhaite avoir des avis.
-                                </br>
-                                </br>
-                                <Strong>Pour aider nos prochains voyageurs, n'hésitez pas à nous donner votre retour sur la destination proposé par mobiliT </strong> 
+                                    </br>
+                                        Même après votre voyage, le personnel de mobiliT reste a votre écoute, en cas d'oublie, ou d'imprevu subi lors de votre voyage.  
+                                    </br>
+                                    </br>
+                                        Nous espérons que votre voyage s'est très bien passé et que vous avez été satisfait de nos renseignements, conseils, ainsi que nos destinations,
+                                        c'est pour cela que moibiliT souhaite avoir des avis.
+                                    </br>
+                                    </br>
+                                    <Strong>Pour aider nos prochains voyageurs, n'hésitez pas à nous donner votre retour sur la destination proposé par mobiliT </strong> 
                                 </p>
                             </div>
                         </a>
@@ -134,110 +140,96 @@
                 </div>
             </div>
         </div>
-       
-    
-    <!-- Footer -->
-
-    <footer>
-   
-
-        <!-- ligne de séparation -->
-        <hr class="hrsep">
-
+            
+        <!-- Footer -->
+        <footer>
+            <!-- ligne de séparation -->
+            <hr class="hrsep">
             <!-- image footer -->
-                <div id="foot" class="img-fluid">
-                   
-
-
+            <div id="foot" class="img-fluid">
                 <!-- colonnes -->
-                    <div class="contact " >
-                        <div class="row">
+                <div class="contact " >
+                    <div class="row">
+                        <!-- colonne 1 -->
+                        <div class="col-4 col-lg-4 text-center p-2">
+                            <h5>Navigation</h5>
+                            <hr class="hrcon">
+                            <p>
+                                <a href="orga.php">
+                                    Accueil
+                                </a>
+                            </p>
+                            <p>
+                                <a href="orga.php">
+                                    Organisation
+                                </a>
+                            </p>
+                            <p>
+                                <a href="#">
+                                    Destinations
+                                </a>
+                            </p>
+                            <p>
+                                <a href="#">
+                                    Forum
+                                </a>
+                            </p>
+                        </div>
 
-                            <!-- colonne 1 -->
-                                <div class="col-4 col-lg-4 text-center p-2">
-                                Navigation
-                                        <hr class="hrcon">
-                                        <p></p>
-                                        <a href="orga.php">
-                                            Accueil
-                                        </a>
-                                        <p></p>
-                                        <a href="orga.php">
-                                            Organisation
-                                        </a>
-                                        </p>
-                                        <p>
-                                        <a href="#">
-                                            Destinations
-                                        </a>
-                                        </p>
-                                        <p>
-                                        <a href="#">
-                                            Forum
-                                        </a>
-                                        </p>
-                                    
-                                </div>
-                        
+                        <!-- colonne 2 -->
+                        <div class="col-4 col-lg-4 text-center p-2 text-black-100">
+                            <h5>Aide</h5>
+                            <hr class="hrcon">
+                                <p>
+                                    <a href="#">
+                                    Personnel
+                                    </a>
+                                </p>
+                                <p>
+                                    <a href="#">
+                                        Recherche
+                                    </a>
+                                </p>
+                                <p>
+                                    <a href="#">
+                                        s'inscrire
+                                    </a>
+                                </p>
+                                <p>
+                                    <a href="#">
+                                        Votre compte
+                                    </a>
+                                </p>
+                            </div>
 
-                                <!-- colonne 2 -->
-                                <div class="col-4 col-lg-4 text-center p-2 text-black-100">
-                                    Aide
-                                        <hr class="hrcon">
-                                            <p>
-                                                <a href="#">
-                                                Personnel
-                                                </a>
-                                            </p>
-                                            <p>
-                                                <a href="#">
-                                                    Recherche
-                                                </a>
-                                            </p>
-                                            <p>
-                                                <a href="#">
-                                                    s'inscrire
-                                                </a>
-                                            </p>
-                                            <p>
-                                                <a href="#">
-                                                    Votre compte
-                                                </a>
-                                            </p>
-                                        
-                                </div>
-
-                                <!-- colonne 3 -->
-                                <div class="col-4 col-lg-4 text-center p-2 text-black-100">
-                                        CONTACT
-                                        <hr class="hrcon">
-                                                <p class="text-center">mobiliT</p>
-                                                <p class="text-center"><i class="fas fa-map-marker-alt pr-3"></i>79 Rue de Marseille, 75010 Paris</p>
-                                                <p class="text-center"><i class="fas fa-phone-square-alt pr-3"></i>0 123 456 789 </p>
-                                                <p class="text-center"><i class="fas fa-at pr-3"></i>mobiliT@pro.fr </p>
-                                </div>
+                            <!-- colonne 3 -->
+                            <div class="col-4 col-lg-4 text-center p-2 text-black-100">
+                                <h5>Contact</h5>
+                                <hr class="hrcon">
+                                <p class="text-center">mobiliT</p>
+                                <p class="text-center"><i class="fas fa-map-marker-alt pr-3"></i>79 Rue de Marseille, 75010 Paris</p>
+                                <p class="text-center"><i class="fas fa-phone-square-alt pr-3"></i>0 123 456 789 </p>
+                                <p class="text-center"><i class="fas fa-at pr-3"></i>mobiliT@pro.fr </p>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        </footer>
 
-
-
-    </footer>
-
-
-<script 
-    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
-    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" 
-    crossorigin="anonymous">
-</script>
-<script 
-    src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
-    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
-    crossorigin="anonymous">
-</script>
-<script 
-    type="text/javascript" 
-    src="script.js">
-</script>
-</body>
+        <script 
+            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
+            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" 
+            crossorigin="anonymous">
+        </script>
+        <script 
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
+            crossorigin="anonymous">
+        </script>
+        <script 
+            type="text/javascript" 
+            src="script.js">
+        </script>
+    </body>
 </html>
