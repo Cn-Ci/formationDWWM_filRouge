730b303f757c946326bd06f767e1f5851c7ba832
Perrine L <perrine.leguai@gmail.com>
2020-11-23 10:31:13 +0100

    destiantion

--- destinations.php
+++ destinations.php
@@ -14,12 +14,7 @@
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
             integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
             crossorigin="anonymous">
-        <!-- JQUERY -->
-        <script
-            src         ="https://code.jquery.com/jquery-3.3.1.min.js"
-            integrity   ="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-            crossorigin ="anonymous">
-        </script>
+        
         <!-- CSS -->
         <link 
             rel="stylesheet" 
@@ -593,6 +588,13 @@
             </div>
         </footer>
 
+        <!-- JQUERY -->
+        <script
+            src         ="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity   ="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin ="anonymous">
+        </script>
+        
         <!-- JAVASCRIPT -->
         <script 
             src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
